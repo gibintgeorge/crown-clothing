6073bc1f733ec384cc68ae61a1b858c07b9c7695
Gibin T George <gibintgeorge@hotmail.com>
2020-06-11 02:42:22 +0530

    Before Routing

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import HomePage from "./pages/HomePage/homepage.component";
 
+import {Route} from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
