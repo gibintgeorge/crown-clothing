aa60f7857bce7ca2de7dc4f9f1352cbeb5006a07
Gibin T George <gibintgeorge@hotmail.com>
2020-06-12 14:51:03 +0530

    Signin completed with firebase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from "./components/header/header.component";
 import SignInandSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js";
 import "./App.css";
 
-import { auth } from "./firebase/firebase.util";
+import { auth, createUserProfileDocumnet } from "./firebase/firebase.util";
 
 class App extends React.Component {
   constructor() {
@@ -16,21 +16,35 @@ class App extends React.Component {
       currentUser: null,
     };
   }
-  unsubscribeFromAuth=null;
-componentDidMount(){
- this.unsubscribeFromAuth= auth.onAuthStateChanged(user=>{
-    this.setState({
-      currentUser:user
-    })
-  })
-}
-componentWillUnmount(){
-  this.unsubscribeFromAuth();
-}
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocumnet(userAuth);
+        console.log(userRef);
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+      }
+      else {//if user is not available then set to logoff
+        this.setState({
+          currentUser: userAuth// can be set to null also
+        })
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -12,6 +12,30 @@ const config = {
   appId: "1:1076790794606:web:20db6cfba0160be68b497f",
   measurementId: "G-324WB2N57J",
 };
+
+export const createUserProfileDocumnet = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshotRef = await userRef.get();
+
+
+  if (!snapshotRef.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch (error) {
+      console.log('Error creating User ', error.message);
+    }
+
+  }
+  return userRef;
+};
 //   Initialize Firebase
 firebase.initializeApp(config);
 //   firebase.analytics();
@@ -20,7 +44,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 export default firebase;
